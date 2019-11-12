2486261a4de4f90ae1b1af4982297392f23bbfe8
Michel Subotkiewiez <michel@mac-pro-de-michel.home>
2019-11-13 00:34:13 +0100

    Connexion User

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -38,7 +38,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function supports(Request $request)
     {
-        return 'app_login' === $request->attributes->get('_route')
+        return 'security_login' === $request->attributes->get('_route')
             && $request->isMethod('POST');
     }
 
